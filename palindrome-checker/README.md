cf2fdb7465d9bc61854e60823355d7d8f9b26e2a
Sunny Zhang <79604811+Zsunnyfc1@users.noreply.github.com>
2022-01-16 23:41:05 -0500

    Add files via upload

---
+++ palindrome-checker/README.md
@@ -0,0 +1,5 @@
+# Palindrome Checker
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/zsunnyfc1/pen/zYEbBGr](https://codepen.io/zsunnyfc1/pen/zYEbBGr).
+
+
---
+++ palindrome-checker/script.js
@@ -0,0 +1,29 @@
+var button = document.getElementById("ConfirmButton");
+
+button.onclick = function myFunction(){
+  var value = document.getElementById("testInput").value;
+    printInput();
+   palindrome(value);
+}
+
+  
+  function printInput() {
+    var value = document.getElementById("testInput").value;
+    document.getElementById("printedStatement").innerHTML = value;
+
+}
+
+
+function palindrome(string) {
+ var re = /[^A-Za-z0-9]/g;
+ string = string.toLowerCase().replace(re, '');
+ var len = string.length;
+ for (var i = 0; i < len/2; i++) {
+   if (str[i] !== str[len - 1 - i]) {
+     document.getElementById("trueorfalse").innerHTML="false";
+       return false;
+   }
+ }
+  document.getElementById("trueorfalse").innerHTML="true";
+ return true;
+}
